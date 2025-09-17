7374c0a68e8b33cbc8f5b7771a5e5a807fd350f6
ViraHrosheva <vdotsenko1991@gmail.com>
2025-09-17 22:28:20 +0300

    updated second_member.py with functions

--- second_member.py
+++ second_member.py
@@ -1,12 +1,21 @@
 # 5-цілочисельне ділення(//), 6-остача від ділення(%), 7-піднести в ступінь, 8-видобути корінь
-def integer_division():
-    pass
+def integer_division(a, b):
+    if b == 0:
+        return "Помилка: ділення на нуль!"
+    return a // b
 
-def remainder_of_dividing():
-    pass
 
-def power():
-    pass
+def remainder_of_dividing(a, b):
+    if b == 0:
+        return "Помилка: ділення на нуль!"
+    return a % b
 
-def root():
-    pass
+
+def power(a, b):
+    return a ** b
+
+
+def root(a, n=2):
+    if a < 0 and n % 2 == 0:
+        return "Помилка: не можна видобути парний корінь з від’ємного числа!"
+    return a ** (1 / n)
