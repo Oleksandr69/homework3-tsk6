9db911b2acd2319c9afb3cc134c5c7d0cdb58811
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-09-18 18:45:45 +0300

    third member commit

--- first_member.py
+++ first_member.py
@@ -3,29 +3,29 @@ def add():
     number1 = int(input("Введіть x: "))
     number2 = int(input("Введіть y: "))
     result = number1 + number2
-    print(f"{number1} + {number2} = {result}")
+    print(f"   {number1} + {number2} = {result}")
 
 def subtraction():
     print("  x - y = ?")
     number1 = int(input("Введіть x: "))
     number2 = int(input("Введіть y: "))
     result = number1 - number2
-    print(f"{number1} - {number2} = {result}")
+    print(f"   {number1} - {number2} = {result}")
 
 def multiplication():
     print("  x * y = ?")
     number1 = int(input("Введіть x: "))
     number2 = int(input("Введіть y: "))
     result = number1 * number2
-    print(f"{number1} * {number2} = {result}")
+    print(f"   {number1} * {number2} = {result}")
 
 def division():
     print("  x / y = ?")
     number1 = int(input("Введіть x: "))
     number2 = int(input("Введіть y: "))
     if number2 == 0:
-        print("На жаль це буде нескінченність!")
+        print("   На жаль це буде нескінченність!")
     else:
         result = number1 / number2
-        print(f"{number1} / {number2} = {result}")
+        print(f"   {number1} / {number2} = {result}")
 
--- third_member.py
+++ third_member.py
@@ -1,12 +1,27 @@
+import math
 # 9-sin(x), 10-cos(x), 11-tng(x), 12-чи парне x
 def sin():
-    pass
+    print("  sin(x) = ?")
+    number = int(input("Введіть x: "))
+    result = math.sin(number)
+    print(f"   sin({number}) = {result}")
 
 def cos():
-    pass
+    print("  cos(x) = ?")
+    number = int(input("Введіть x: "))
+    result = math.cos(number)
+    print(f"   cos({number}) = {result}")
 
 def tg():
-    pass
+    print("  tg(x) = ?")
+    number = int(input("Введіть x: "))
+    result = math.tan(number)
+    print(f"   tg({number}) = {result}")
 
 def is_even():
-    pass
+    print("is x even ?")
+    number = int(input("Введіть x: "))
+    if number % 2 == 0:
+        print(f"   {number} is even.")
+    else:
+        print(f"   {number} is not even.")
